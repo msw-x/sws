cd2778da8fda45908997910b514e46e0ddec41e6
msw <mswl@ya.ru>
2023-04-05 15:49:51 +0300

    cert host from env

--- src/run.go
+++ src/run.go
@@ -3,11 +3,15 @@ package main
 import (
 	"io/ioutil"
 	"log"
+	"os"
 
 	"github.com/msw-x/moon/app"
 )
 
 func run(conf Conf) {
+	if certHost, ok := os.LookupEnv("CERT_HOST"); ok {
+		conf.CertHost = certHost
+	}
 	log.SetOutput(ioutil.Discard)
 	srv := New(conf)
 	defer srv.Close()
