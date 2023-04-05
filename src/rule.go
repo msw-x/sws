a8b65f67469fc628321cacd290b8da67bb7da636
msw <mswl@ya.ru>
2023-04-05 16:03:25 +0300

    routes from env

--- src/rule.go
+++ src/rule.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
 	"github.com/msw-x/moon/fs"
 	"github.com/msw-x/moon/ustring"
@@ -22,17 +24,25 @@ func (o Rule) String() string {
 
 func loadRules(file string) (rules []Rule, err error) {
 	var lines []string
+	add := func(line string) {
+		line = ustring.TrimWhitespaces(line)
+		if line != "" {
+			var rule Rule
+			rule.Source, rule.Target = ustring.SplitPair(line, ":")
+			if rule.Ok() {
+				rules = append(rules, rule)
+			}
+		}
+	}
 	lines, err = fs.ReadLines(file)
 	if err == nil {
 		for _, line := range lines {
-			line = ustring.TrimWhitespaces(line)
-			if line != "" {
-				var rule Rule
-				rule.Source, rule.Target = ustring.SplitPair(line, ":")
-				if rule.Ok() {
-					rules = append(rules, rule)
-				}
-			}
+			add(line)
+		}
+	}
+	if s, ok := os.LookupEnv("ADD_ROUTES"); ok {
+		for _, v := range strings.Split(s, ",") {
+			add(v)
 		}
 	}
 	return
