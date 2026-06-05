a5391743fd1e21ece63eb661b6510325e0632a66
msw <msw.x@ya.ru>
2026-06-05 17:42:07 +0300

    Revert "fix target"
    This reverts commit 5c83371ed9b97d15108ed2497486e17d29c23ede.

--- src/proxy.go
+++ src/proxy.go
@@ -24,11 +24,7 @@ func NewProxy(targetUrl string, log *ulog.Log) (*httputil.ReverseProxy, error) {
 
 func NewHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if target := mux.Vars(r)["target"]; target == "" {
-			r.URL.Path = "/"
-		} else {
-			r.URL.Path = target
-		}
+		r.URL.Path = mux.Vars(r)["target"]
 		proxy.ServeHTTP(w, r)
 	}
 }
